b11bd05c412d326b3c8d738e5364a81449fd37df
{haini000} <{haini00)@naver.com}>
2025-07-23 09:34:53 +0900

    feat: 테스트 코드

--- src/test/java/com/nhnacademy/illuwa/domain/payment/controller/PaymentControllerTest.java
+++ src/test/java/com/nhnacademy/illuwa/domain/payment/controller/PaymentControllerTest.java
@@ -1,6 +1,8 @@
 package com.nhnacademy.illuwa.domain.payment.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nhnacademy.illuwa.domain.payment.dto.PaymentConfirmRequest;
+import com.nhnacademy.illuwa.domain.payment.dto.PaymentRefundRequest;
 import com.nhnacademy.illuwa.domain.payment.dto.PaymentResponse;
 import com.nhnacademy.illuwa.domain.payment.service.PaymentService;
 import org.junit.jupiter.api.Disabled;
@@ -10,15 +12,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Disabled
 class PaymentControllerTest {
 
     @Autowired
@@ -30,34 +36,78 @@ class PaymentControllerTest {
     @MockBean
     private PaymentService paymentService;
 
-    @Test
-    @DisplayName("GET /v1/payments/orders/{orderId} - 조회 성공")
-    void findPaymentByOrderId_success() throws Exception {
+    private PaymentResponse createMockPaymentResponse(String orderId, String paymentKey, int totalAmount, String status, String orderName) {
+        PaymentResponse.CardInfo cardInfo = new PaymentResponse.CardInfo();
+        cardInfo.setIssuerCode("11");
+        cardInfo.setAcquirerCode("22");
+        cardInfo.setNumber("123456");
+        cardInfo.setCardType("VISA");
+        cardInfo.setAmount(totalAmount);
+
         PaymentResponse response = new PaymentResponse();
-        response.setOrderId("order-123");
-
-        when(paymentService.findPaymentByOrderId("order-123")).thenReturn(response);
-
-        mockMvc.perform(get("/v1/payments/orders/order-123"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.orderId").value("order-123"));
+        response.setPaymentKey(paymentKey);
+        response.setOrderId(orderId);
+        response.setOrderName(orderName);
+        response.setStatus(status);
+        response.setTotalAmount(totalAmount);
+        response.setApprovedAt(OffsetDateTime.now(ZoneOffset.UTC));
+        response.setRequestedAt(OffsetDateTime.now(ZoneOffset.UTC));
+        response.setCard(cardInfo);
+        return response;
     }
 
     @Test
-    @DisplayName("GET / - 결제 페이지")
-    void index_success() throws Exception {
-        mockMvc.perform(get("/"))
+    @DisplayName("api/payments/confirm")
+    void confirmPaymentTest() throws Exception {
+
+        PaymentConfirmRequest request = new PaymentConfirmRequest("test-order-id", "test-payment-key", 1000);
+
+        PaymentResponse expectedResponse = createMockPaymentResponse(
+                request.getOrderNumber(),
+                request.getPaymentKey(),
+                request.getAmount(),
+                "DONE",
+                "test payment order"
+        );
+        when(paymentService.confirm(any(PaymentConfirmRequest.class))).thenReturn(expectedResponse);
+
+        mockMvc.perform(post("/api/payments/confirm")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isOk())
-                .andExpect(view().name("/payment/checkout"));
+                .andExpect(jsonPath("$.orderId").value(request.getOrderNumber()))
+                .andExpect(jsonPath("$.paymentKey").value(request.getPaymentKey()))
+                .andExpect(jsonPath("$.totalAmount").value(request.getAmount()))
+                .andExpect(jsonPath("$.status").value(expectedResponse.getStatus()));
+
     }
 
     @Test
-    @DisplayName("GET /callback-auth - 인증 응답")
-    void callbackAuth_success() throws Exception {
-        mockMvc.perform(get("/callback-auth")
-                        .param("customerKey", "key123")
-                        .param("code", "auth-code"))
-                .andExpect(status().isOk()); // 실제 동작 확인은 통신 모킹 필요
+    @DisplayName("/api/payments/refund")
+    void refundPaymentTest() throws Exception {
+        PaymentRefundRequest testRequest = new PaymentRefundRequest("test-order-id", "test");
+
+        PaymentResponse testResponse = createMockPaymentResponse(
+                testRequest.getOrderNumber(),
+                "test-refund-payment-ket",
+                1000,
+                "CANCELED",
+                "test payment refund"
+        );
+        when(paymentService.cancelPayment(any(PaymentRefundRequest.class))).thenReturn(testResponse);
+
+        mockMvc.perform(post("/api/payments/refund")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(testRequest)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.orderId").value(testResponse.getOrderId()))
+                .andExpect(jsonPath("$.paymentKey").value(testResponse.getPaymentKey()))
+                .andExpect(jsonPath("$.totalAmount").value(testResponse.getTotalAmount()))
+                .andExpect(jsonPath("$.status").value(testResponse.getStatus()))
+                .andExpect(jsonPath("$.orderName").value(testResponse.getOrderName()));
     }
+
+
 
 }
+
